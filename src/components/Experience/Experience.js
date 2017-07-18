142a57921bbe3253a358e672f0b4ea7e9289d858
Jesus Castañeda <>
2017-07-17 22:35:05 -0600

    fix details

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -8,16 +8,22 @@ export default class Experience extends Component {
         <h2><i className='fa fa-suitcase' /> EXPERIENCIA</h2>
         <br />
         <hr />
+        
         <div>
-          <p className='company'><i className='fa fa-building' /> Jesco Marketing Monterrey N.L | 2012 - Actual</p>
+          <p className='company'><i className='fa fa-building' /> Gamma Partners Chihuahua, Chihuahua | 2017 - Actual</p>
+          <hr/>
+          <p className='company'><i className='fa fa-building' /> Bridge Studio Guadalajara, Jalisco | 2016 - 2017</p>
+          <hr/>
+          <p className='company'><i className='fa fa-building' /> Jesco Marketing Monterrey N.L | 2012 - 2017</p>
+          <hr/>
           <p className='texto'>
             Como desarrollador web y móvil he trabajado en diferentes proyectos y en cada uno he utilizado la tecnología mas adecuada.
           </p>
           <p className='texto'>
-            Desarrollo mediante React y Angular en base a componentes, utilizando html5, css3, jquery, bootstrap, foundation, javascript es6, entre otras.
+            Desarrollo mediante Angular o React en base a componentes, utilizando html5, css3, jquery, bootstrap, foundation, javascript es6, entre otras.
           </p>
           <p className='texto'>
-            También tengo algo de experiencia en desarrollo móvil utilizando tecnologías como react native y ionic.
+            También tengo experiencia en desarrollo móvil utilizando tecnologías como Ionic y React Native.
           </p>
           <p className='texto'>
             Mi trabajo es pasar a código los diseños en photoshop y realizar la conexión entre las vistas con las apis que utilizamos, tanto api rest creada por nosotros o apis de terceros como facebook, twitter, google maps, spotify, etc...
